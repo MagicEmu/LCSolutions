e41ad77eae0aa2a49f6e6e6027960f85ff7bded5
Yichen Dai <yichendai3@gmail.com>
2021-04-13 13:37:19 -0700

    added more solutions

--- solutions.java
+++ solutions.java
@@ -137,4 +137,183 @@ public class solutions {
         }
         return head;
     }
+	public String[] uncommonFromSentences(String A, String B) {
+        Map<String, Integer> count = new HashMap();
+        for (String word: A.split(" "))
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        for (String word: B.split(" "))
+            count.put(word, count.getOrDefault(word, 0) + 1);
+
+        List<String> ans = new LinkedList();
+        for (String word: count.keySet())
+            if (count.get(word) == 1)
+                ans.add(word);
+
+        return ans.toArray(new String[ans.size()]);
+    }
+	public int tribonacci(int n) {
+        if (n < 2) return n;
+        int a = 0, b = 1, c = 1, d;
+        while (n-- > 2) {
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+	public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet();
+        return find(root, k, set);
+    }
+    public boolean find(TreeNode root, int k, Set < Integer > set) {
+        if (root == null)
+            return false;
+        if (set.contains(k - root.val))
+            return true;
+        set.add(root.val);
+        return find(root.left, k, set) || find(root.right, k, set);
+    }
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]);
+        int boxes = 0;
+        for (int[] box : boxTypes) {
+            if (truckSize >= box[0]) {
+                boxes += box[0] * box[1];
+                truckSize -= box[0];
+            }else {
+                boxes += truckSize * box[1];
+                return boxes;
+            }
+        }
+        return boxes;
+    }
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles;
+        while (numBottles >= numExchange) {
+            int remainder = numBottles % numExchange;
+            numBottles /= numExchange;
+            ans += numBottles;
+            numBottles += remainder;
+        }
+        return ans;
+    }
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num: nums) {
+            set.add(num);
+        }
+        for (int i=0; i<=nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+    public int maximumProduct(int[] nums) {
+        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE, min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+
+            if (n < min1) {
+                min2 = min1;
+                min1 = n;
+            } else if (n < min2) {
+                min2 = n;
+            }
+        }
+        return Math.max(max1*max2*max3, max1*min1*min2);
+    }
+    public int numRookCaptures(char[][] board) {
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                if (board[i][j] == 'R') {
+                    return Count(board, i, j);
+                }
+            }
+        }
+        return 0;
+    }
+    public int Count(char[][] board, int row, int col) {
+        int res = 0;
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        for (int[] d: directions) {
+            for (int i = row, j = col; i >= 0 && i < 8 && j >= 0 && j < 8; i = i + d[0], j = j + d[1]) {
+                if (board[i][j] == 'p') {
+                    res++;
+                    break;
+                } else if (board[i][j] == 'B') {
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+    public String tictactoe(int[][] moves) {
+        int[][] row = new int[2][3], col = new int[2][3];
+        int[] d1 = new int[2], d2 = new int[2];
+        for (int i = 0; i < moves.length; ++i) {
+            int r = moves[i][0], c = moves[i][1], id = i % 2;
+            if (++row[id][r] == 3 || ++col[id][c] == 3 || r == c && ++d1[id] == 3 || r + c == 2 && ++d2[id] == 3) 
+                return id == 0 ? "A" : "B";
+        }
+        return moves.length == 9 ? "Draw" : "Pending";        
+    }
+    public int maxProduct(int[] nums) {
+        int max = 0;
+        int a = 0;
+        int b = 0;
+        for (int i=0; i<nums.length; i++) {
+            a = nums[i]-1;
+            for (int j=0; j<nums.length; j++) {
+                if (i != j) {
+                    b = nums[j]-1;
+                    max = Math.max(max, a*b);
+                }
+            }
+        }
+        return max;
+    }
+    public int numDifferentIntegers(String word) {
+        String[] arr= word.split("\\D");
+        Set<String> s = new HashSet<>();
+        for(String str:arr){
+            str=str.trim();
+            if(!str.equals("")){
+                str = str.replaceAll("^0*","");
+                s.add(str);
+            }
+        }
+        return s.size();
+    }
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+
+        int carry = 0;
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+        while (p1 >= 0 || p2 >= 0) {
+            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
+            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+            int value = (x1 + x2 + carry) % 10;
+            carry = (x1 + x2 + carry) / 10;
+            res.append(value);
+            p1--;
+            p2--;    
+        }
+        
+        if (carry != 0)
+            res.append(carry);
+        
+        return res.reverse().toString();
+    }
 }
